b4ffab668a0815a44d31bb93732b9842e18b5bc1
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2024-07-16 11:29:35 -0300

    Basic scope reading

--- scope.py
+++ scope.py
@@ -4,14 +4,23 @@ from lisptypes import LispSymbol, LispValue
 class Scope:
     """Represents a Dynamic Scope. In order to use the language with a static scope, see `bind_to_static_scope`"""
 
+    def __init__(self) -> None:
+        # Already begins in one level
+        self.scopes: list[list[tuple[LispSymbol, LispValue]]] = [[]]
+
     def read_symbol(self, symbol: LispSymbol) -> LispValue | None:
-        raise Exception("not implemented")
+        # From most specific scope to least specific
+        for scope in reversed(self.scopes):
+            for (name, value) in scope:
+                if symbol == name:
+                    return value
+        return None
 
     def begin_block(self) -> None:
-        raise Exception("not implemented")
+        self.scopes.append([])
 
     def end_block(self) -> None:
-        raise Exception("not implemented")
+        self.scopes.pop()
 
 
 def bind_to_static_scope(ast: list[LispValue]) -> list[LispValue]:
