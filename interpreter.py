c379dfdcd0db73bc6157afbe96604295c6ed6d90
Eduardo Aurélio Halmenschlager Duarte <eahduarte@inf.ufrgs.br>
2024-08-04 11:25:10 -0300

    Added basic arithmetic

--- interpreter.py
+++ interpreter.py
@@ -2,6 +2,7 @@ from lisptypes import LispEmptyList, LispList, LispNonEmptyList, LispNumber, Lis
 from parser import LispValue
 from scope import Scope
 from screen import Screen
+from functools import reduce
 
 
 def eval(ast: list[LispValue], scope: Scope, screen: Screen) -> LispValue:
@@ -41,13 +42,31 @@ def eval_expression(expr: LispValue, scope: Scope, screen: Screen) -> LispValue:
 def eval_function_application(name: LispSymbol, arguments: LispList, scope: Scope, screen: Screen) -> LispValue:
     match name.symbolName:
         case "+":
-            return LispNumber(0)  # TODO
+            operators = arithmetic_helper("addition", arguments, scope, screen)
+            return LispNumber(sum(operators))
         case "-":
-            return LispNumber(0)  # TODO
+            operators = arithmetic_helper("subtraction", arguments, scope, screen)
+            return LispNumber(reduce((lambda x, y: x - y), operators))
         case "*":
-            return LispNumber(0)  # TODO
+            operators = arithmetic_helper("multiplication", arguments, scope, screen)
+            return LispNumber(reduce((lambda x, y: x * y), operators))
         case "/":
-            return LispNumber(0)  # TODO
+            args = arguments.to_python_list()
+            if len(args) != 2:
+                raise Exception(
+                    f"division needs exactly two arguments, but was called with {args}")
+            [op1, op2] = args
+            dividend = eval_expression(op1, scope, screen)
+            divisor = eval_expression(op2, scope, screen)
+
+            if not isinstance(dividend, LispNumber) or not isinstance(divisor, LispNumber):
+                raise Exception(
+                    f"can't perform division using non num values, attempted: {dividend}/{divisor}")
+            if divisor.numberValue == 0:
+                raise Exception(
+                    f"can't divide a number by zero")
+            # Implementing only integer division
+            return LispNumber(dividend.numberValue//divisor.numberValue)
         case "defun":
             return LispEmptyList()  # TODO
         case "let":
@@ -108,3 +127,14 @@ def eval_function_application(name: LispSymbol, arguments: LispList, scope: Scop
             result = LispEmptyList()
             scope.end_block()
             return result
+
+# Returns a list of the n operands for addition, subtraction and multiplication
+def arithmetic_helper(operation: str, arguments: LispList, scope: Scope, screen: Screen) -> list[int]:
+    operators: list[int] = []
+    for expr in arguments.to_python_list():
+        operator = eval_expression(expr, scope, screen)
+        if not isinstance(operator, LispNumber):
+            raise Exception(
+                f"tried to perform {operation} with a not num types: {operator}")
+        operators.append(operator.numberValue)
+    return operators
