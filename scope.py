b8d1825d586e3f0a46968cc275eb6d872a26398b
Eduardo Aurélio Halmenschlager Duarte <eahduarte@inf.ufrgs.br>
2024-08-25 16:16:14 -0300

    Fix static scope

--- scope.py
+++ scope.py
@@ -1,4 +1,4 @@
-from lisptypes import LispSymbol, LispValue, LispList, LispNonEmptyList
+from lisptypes import LispSymbol, LispValue, LispList, LispNonEmptyList, LispNumber, LispEmptyList
 
 
 class Scope:
@@ -51,30 +51,37 @@ def bind_to_static_scope(ast: list[LispValue], scope: Scope) -> list[LispValue]:
         # Variable declaration
         if isinstance(node, LispNonEmptyList) and isinstance(node.first, LispSymbol) and node.first.symbolName == "let":
             args = node.to_python_list()
+            # Well-formed variable declaration structure is (let var expr)
+            operator = args[0] 
             var = args[1]
             expr = args[2]
 
             if not isinstance(var, LispSymbol):
                     raise Exception(
                         f"can't perform attribution using '{var}' as variable")
+            
+            # Creates a new name for the variable
+            # everytime the same variable is found program must replace by the new one
             new_var: LispSymbol = make_var_name(var)
             scope.create_symbol(var, new_var)
     
-            something = LispList.from_list([args[0], new_var] + bind_to_static_scope([expr], scope))
-            result_ast.append(something)
+            # Replace the node by the new modified one
+            new_node = LispList.from_list([operator, new_var] + bind_to_static_scope([expr], scope))
+            result_ast.append(new_node)
         
         # Function declaration
         elif isinstance(node, LispNonEmptyList) and isinstance(node.first, LispSymbol) and node.first.symbolName == "defun":
             scope.begin_block()
             args = node.to_python_list()
-            [_, function_name, args_list, *function_body] = args
+            # Well-formed function declaration is (defun name (args) (body))
+            [operator, function_name, args_list, *function_body] = args
             
             if not isinstance(args_list, LispList):
                     raise Exception(
                         f"function declaration expects list of arguments, given {args_list}")
-            
             new_args_list: list[LispValue] = []
 
+            # Creates a new variable name for every argument in the function
             for function_arg in args_list.to_python_list():
                     if not isinstance(function_arg, LispSymbol):
                         raise Exception(
@@ -83,32 +90,35 @@ def bind_to_static_scope(ast: list[LispValue], scope: Scope) -> list[LispValue]:
                     new_args_list.append(new_var)
                     scope.create_symbol(function_arg, new_var)
 
-            something = LispList.from_list([args[0], function_name, LispList.from_list(new_args_list)] + bind_to_static_scope(function_body, scope))
-            result_ast.append(something)
+            new_node = LispList.from_list([operator, function_name, LispList.from_list(new_args_list)] + bind_to_static_scope(function_body, scope))
+            result_ast.append(new_node)
             scope.end_block()
 
         # Function application
         elif isinstance(node, LispNonEmptyList) and isinstance(node.first, LispSymbol):
             args = node.to_python_list()
+            # Well-formed function application is (name arguments)
             [function_name, *args_list] = args
             renamed_application = [function_name]
 
-            for function_arg in args_list:
-                # This is a function application, the user can pass values or variables as parameters
-                # We must check if is a variable and replace it
-                if isinstance(function_arg, LispSymbol):
-                    replaced_name = scope.read_symbol(function_arg)
-
-                    if replaced_name is None:
-                        raise Exception(f"variable name {function_arg} not defined")
-                    
-                    renamed_application.append(replaced_name)
-                else:
-                    # I have absolutely no ideia why I need to use indexing for this, I'm too tired bro...
-                    renamed_application.append(bind_to_static_scope([function_arg], scope)[0])
+            renamed_application.extend(bind_to_static_scope(args_list, scope))
             result_ast.append(LispList.from_list(renamed_application))
-            
+        
+        elif isinstance(node, LispSymbol):
+            replaced_name = scope.read_symbol(node)
+            if replaced_name is None:
+                    raise Exception(f"variable name {node} not defined")
+            result_ast.append(replaced_name)
+
+        elif isinstance(node, LispNumber):
+            result_ast.append(node)
+        
+        elif isinstance(node, LispEmptyList):
+            result_ast.append(node)
+
         else:
-             result_ast.append(node)
+            raise Exception(f"unexpected syntax {node}")
+
     return result_ast
+    
                 
